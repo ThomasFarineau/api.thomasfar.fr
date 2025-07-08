1dcde0239b99c50930c0c8989e98318020f899a2
Thomas Farineau <thomas.farineau@icloud.com>
2025-07-08 15:21:30 +0200

    feat: test update

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,12 @@ const app = express();
 app.use(express.json());
 
 // Votre endpoint d'update
-app.use('/update', update);
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello world</h1>');
 });
+
+app.use('/update', update);
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 app.listen(PORT, () => {
