3b2d4494f3358b5a339f4307c3bac47682a2c41b
Thomas Farineau <thomas.farineau@icloud.com>
2025-07-08 16:32:06 +0200

    feat: replace dotenv with config for environment variables and add default configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,8 @@ module.exports = {
     apps: [{
         name: 'api',
         script: 'dist/index.js',
-        env: {
-            NODE_ENV: 'production',
-            PORT: 3000,
-            UPDATE_TOKEN: process.env.UPDATE_TOKEN
+        env_production: {
+            NODE_ENV: 'production'
         }
     }]
 };
